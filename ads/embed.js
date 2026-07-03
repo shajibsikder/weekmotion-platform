7178504bb47b258221ac0347307725f0a21de40b
Md Shajib sikder <127028992+shajibsikder@users.noreply.github.com>
2026-07-03 20:41:39 +0600

    Update embed.js

--- ads/embed.js
+++ ads/embed.js
@@ -1,19 +1,19 @@
-(async () => {
+(async function () {
 
-    // CSS
-    const css = document.createElement("link");
-    css.rel = "stylesheet";
-    css.href = "https://cdn.jsdelivr.net/gh/shajibsikder/weekmotion-platform@main/ads/banner.css";
-    document.head.appendChild(css);
+    // CSS Load
+    if (!document.getElementById("wm-banner-css")) {
+        const css = document.createElement("link");
+        css.id = "wm-banner-css";
+        css.rel = "stylesheet";
+        css.href = "https://cdn.jsdelivr.net/gh/shajibsikder/weekmotion-platform@main/ads/banner.css";
+        document.head.appendChild(css);
+    }
 
-    // HTML
+    // HTML Load
     const html = await fetch(
-        "https://cdn.jsdelivr.net/gh/shajibsikder/weekmotion-platform@main/ads/banner.html"
+        "https://cdn.jsdelivr.net/gh/shajibsikder/weekmotion-platform@main/ads/banner.html?v=" + Date.now()
     ).then(r => r.text());
-
-    const div = document.createElement("div");
-    div.innerHTML = html;
-
-    document.body.prepend(div);
+    
+    document.currentScript.insertAdjacentHTML("beforebegin", html);
 
 })();
