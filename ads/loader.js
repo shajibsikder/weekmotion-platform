30540d9a6f5103aaf98b935787536b459c224464
Md Shajib sikder <127028992+shajibsikder@users.noreply.github.com>
2026-07-03 21:07:37 +0600

    Delete ads directory

--- ads/loader.js
+++
@@ -1,6 +0,0 @@
-fetch("https://cdn.jsdelivr.net/gh/shajibsikder/weekmotion-platform@main/ads/banner.html")
-.then(r => r.text())
-.then(html => {
-    document.querySelector(".weekmotion-ad").innerHTML = html;
-})
-.catch(err => console.log(err));
