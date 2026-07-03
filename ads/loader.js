5bdc515250dcc67ea74bb5545c716f76e54a42cf
Md Shajib sikder <127028992+shajibsikder@users.noreply.github.com>
2026-07-03 20:59:04 +0600

    Update loader.js

--- ads/loader.js
+++ ads/loader.js
@@ -1,13 +1,7 @@
-(async () => {
-    const target = document.currentScript.previousElementSibling;
-
-    if (!target) return;
-
-    const shadow = target.attachShadow({ mode: "open" });
-
-    const html = await fetch(
-        "https://cdn.jsdelivr.net/gh/shajibsikder/weekmotion-platform@main/ads/banner.html?"+Date.now()
-    ).then(r => r.text());
-
-    shadow.innerHTML = html;
-})();
+fetch("https://cdn.jsdelivr.net/gh/shajibsikder/weekmotion-platform@main/ads/banner.html?" + Date.now())
+.then(r=>r.text())
+.then(html=>{
+    document.querySelectorAll(".weekmotion-ad").forEach(el=>{
+        el.innerHTML=html;
+    });
+});
